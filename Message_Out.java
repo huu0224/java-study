45391a54beb11236e8b2e83008ccaa3b8794f1c5
huu0224 <huu0224@hs.ac.kr>
2025-12-18 18:31:56 +0900

    System.out.println을 활용한 기본 콘솔 정보 출력 실습
    자바의 표준 출력 메서드인 System.out.println을 사용하여 격언, 현재 날짜 및 시간, 그리고 개인 신상 정보(이름, 나이, 위치)를 화면에 순차적으로 출력하는 기초 프로그램입니다.

---
+++ Message_Out.java
@@ -0,0 +1,15 @@
+public class Message_Out {
+
+	public static void main(String[] args) {
+        // (1) 좋아하는 격언 출력
+        System.out.println("격언: 꾸준함은 재능을 이긴다.");
+
+        // (2) 오늘의 날짜와 시간 출력
+        System.out.println("오늘의 날짜와 시간: 2025.12.17 수요일");
+
+        // (3) 이름, 나이, 현위치 출력
+        System.out.println("이름: 허윤정");
+        System.out.println("나이: 23세");
+        System.out.println("현위치: 대한민국");
+    }
+}
