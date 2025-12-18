30a83cc31bfc90450950bc71a5bf2e2d98bd9d6f
huu0224 <huu0224@hs.ac.kr>
2025-12-18 18:06:17 +0900

    Java 기반 도서 정보 출력 프로그램
    본 프로그램은 Java 언어를 활용하여 도서 정보를 출력하는 기능을 구현한 예제이다. 도서와 관련된 정보를 클래스 형태로 관리하고, 메서드를 통해 필요한 데이터를 출력하도록 구성하여 객체지향 프로그래밍의 기본 개념을 이해하는 것을 목표로 한다. 이를 통해 Java의 클래스 구조와 데이터 처리 방식에 익숙해질 수 있도록 설계되었다.

---
+++ BookInfo_Out.java
@@ -0,0 +1,13 @@
+
+public class BookInfo_Out {
+
+	public static void main(String[] args) {
+
+        String bookInfo =
+                "책 어린 왕자는 앙투안 드 생텍쥐페리가 지은 소설로, " +
+                "출판사는 문학동네이며 가격은 약 12,000원이다. " +
+                "전체 페이지 수는 약 200페이지로, 삶과 인간관계에 대한 깊은 메시지를 담고 있다.";
+
+        System.out.println(bookInfo);
+    }
+}
