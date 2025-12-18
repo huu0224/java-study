bc3f67f013a7e4ebeb6c266cf880dcb6ef0b8d7a
huu0224 <huu0224@hs.ac.kr>
2025-12-18 19:08:49 +0900

    String 변수를 활용한 정적 텍스트 데이터 기록 및 콘솔 출력 실습
    문자열 자료형인 String 변수에 인물, 과자, 영화, 제품, 언어 등 다양한 카테고리의 정보를 문장 형태로 저장하고, 이를 System.out.println을 통해 순차적으로 출력하는 기초 실습 코드입니다.

---
+++ String_Out.java
@@ -0,0 +1,32 @@
+
+public class String_Out {
+
+	 public static void main(String[] args) {
+
+	        // 예제
+	        String person =
+	                "홍길동은 18세이며 금강산에 있다.";
+	        System.out.println(person);
+
+	        // (1) 과자 정보
+	        String snack =
+	                "새우깡은 1971년에 출시된 농심의 과자이다.";
+	        System.out.println(snack);
+
+	        // (2) 영화 정보
+	        String movie =
+	                "매트릭스는 SF와 액션 장르의 영화로, 감독은 릴리 워쇼스키와 라나 워쇼스키이며 " +
+	                "주연배우로는 키아누 리브스, 로렌스 피시번, 캐리 앤 모스, 휴고 위빙이 출연한다.";
+	        System.out.println(movie);
+
+	        // (3) 제품 정보
+	        String product =
+	                "Photoshop은 Adobe System에서 제작한 graphic S/W이다.";
+	        System.out.println(product);
+
+	        // (4) 언어 정보
+	        String language =
+	                "Java는 1995년에 발표된 프로그래밍 언어로, 제작자는 James Gosling이다.";
+	        System.out.println(language);
+	    }
+	}
