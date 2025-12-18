fe7e2e1cd599c26354b9087a1e9a49e98d23548f
huu0224 <huu0224@hs.ac.kr>
2025-12-18 19:09:45 +0900

    자바 소스 코드의 구성 요소(토큰) 분석 및 주석 실습
    자바 프로그램의 기본 실행 구조를 바탕으로 키워드, 식별자(클래스/메소드/변수 이름), 연산자, 리터럴(상수), 분리자 등 코드를 이루는 최소 단위인 '토큰(Token)'의 역할을 주석으로 상세히 분류하고 분석한 예제입니다.

---
+++ Token_Test.java
@@ -0,0 +1,37 @@
+
+public class Token_Test {
+
+	public static void main(String[] args) {
+		// public[키워드] static[키워드] void[키워드]
+	    // main[메소드 이름]
+	    // ([분리자] String[클래스 이름] [] [분리자] args[변수 이름] )[분리자]
+	    // {[분리자]
+
+	        String name = "홍길동";
+	        // String[클래스 이름]
+	        // name[변수 이름]
+	        // =[연산자]
+	        // "홍길동"[문자열 상수]
+	        // ;[분리자]
+
+	        int age = 18;
+	        // int[키워드]
+	        // age[변수 이름]
+	        // =[연산자]
+	        // 18[정수 상수]
+	        // ;[분리자]
+
+	        System.out.println(name);
+	        // System[클래스 이름]
+	        // .[분리자]
+	        // out[변수 이름]
+	        // .[분리자]
+	        // println[메소드 이름]
+	        // ([분리자]
+	        // name[변수 이름]
+	        // )[분리자]
+	        // ;[분리자]
+
+	    }   // }[분리자]
+
+	}       // }[분리자]
