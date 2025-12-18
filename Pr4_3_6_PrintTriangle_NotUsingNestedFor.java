9304f015a57dc298f5f01aa48a879f2a97c97516
huu0224 <huu0224@hs.ac.kr>
2025-12-18 19:05:46 +0900

    중첩 반복문을 사용하지 않은 고정 출력 방식의 직각 삼각형 그리기
    for 문과 같은 반복 제어문을 활용하는 대신, System.out.println 메소드를 각 행마다 직접 호출하여 별(*) 기호로 구성된 밑변 길의 5인 직각 삼각형 형상을 콘솔에 정적으로 출력하는 기초 예제입니다.

---
+++ Pr4_3_6_PrintTriangle_NotUsingNestedFor.java
@@ -0,0 +1,13 @@
+public class Pr4_3_6_PrintTriangle_NotUsingNestedFor {
+
+	public static void main(String[] args) {
+		
+        System.out.println("\n  << '*'로 그린 밑변 5인 직각 삼각형 >>\n");
+
+        System.out.println("  *");
+        System.out.println("  **");
+        System.out.println("  ***");
+        System.out.println("  ****");
+        System.out.println("  *****");
+    }
+}
