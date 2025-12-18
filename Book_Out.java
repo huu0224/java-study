3e7a633a2dc4358cdb7c4d5c7813b90f1726918f
huu0224 <huu0224@hs.ac.kr>
2025-12-18 18:04:36 +0900

    Java를 이용한 도서 대출 관리 프로그램
    본 프로그램은 Java 언어를 사용하여 도서의 대출 정보를 관리하는 기능을 구현한 예제이다. 도서 대출과 관련된 기본적인 처리 과정을 객체지향 개념을 바탕으로 구성하였으며, 클래스와 메서드를 활용하여 프로그램의 구조를 명확히 하였다. 이를 통해 Java의 기본 문법과 객체지향 프로그래밍 개념을 이해하는 것을 목표로 한다.

---
+++ Book_Out.java
@@ -0,0 +1,18 @@
+
+public class Book_Out {
+
+	public static void main(String[] args) {
+
+        String bookTitle = "어린 왕자";
+        String author = "앙투안 드 생텍쥐페리";
+        String publisher = "문학동네";
+        int price = 12000;
+        int pageCount = 200;
+
+        System.out.println("책 제목: " + bookTitle);
+        System.out.println("저자: " + author);
+        System.out.println("출판사: " + publisher);
+        System.out.println("가격: " + price + "원");
+        System.out.println("페이지 수: " + pageCount);
+    }
+}
