a79b63fe5686049fd552f0ccbf95f41b748c1c25
huu0224 <huu0224@hs.ac.kr>
2025-12-18 19:01:53 +0900

    변수 선언과 초기화를 이용한 도서 정보 출력 실습
    문자열(String)과 정수형(int) 자료형 변수를 선언함과 동시에 특정 도서의 제목, 저자, 가격 등의 데이터를 할당하고 이를 System.out.println으로 조합하여 출력하는 기본 예제입니다.

---
+++ Pr2_2_1_Book_Initialization_Out.java
@@ -0,0 +1,21 @@
+
+public class Pr2_2_1_Book_Initialization_Out {
+
+	public static void main(String[] args) {
+		
+		   // 책 정보 변수 선언 및 초기화
+        String bookTitle = "어린 왕자";
+        String author = "앙투안 드 생텍쥐페리";
+        String publisher = "문학동네";
+        int price = 12000;
+        int pages = 160;
+
+        // 출력
+        System.out.println("** 좋아하는 책 정보 **");
+        System.out.println("제목: " + bookTitle);
+        System.out.println("저자: " + author);
+        System.out.println("출판사: " + publisher);
+        System.out.println("가격: " + price + "원");
+        System.out.println("페이지 수: " + pages + "쪽");
+    }
+}
