8f77e55c859af3ad4e47143de08dd6f274529a00
huu0224 <huu0224@hs.ac.kr>
2025-12-20 11:49:03 +0900

    Java를 이용한 직각삼각형 판별 프로그램
    본 프로그램은 Java 언어를 사용하여 세 변의 길이를 입력받고, 대입(Substituting) 방법을 이용해 해당 삼각형이 직각삼각형인지 판별하는 예제이다. 조건문과 연산자를 활용하여 직각삼각형의 성립 조건을 확인함으로써 기본적인 수학적 로직 구현과 제어문 활용 능력을 학습하는 것을 목표로 한다.

---
+++ Ex5_2_6_RightTriangle_UsingSubstitutingMethod.java
@@ -0,0 +1,66 @@
+public class Ex5_2_6_RightTriangle_UsingSubstitutingMethod {
+
+	public static void main(String[] args) {
+        printStar1InLine(); 
+        printStar2InLine();  
+        printStar3InLine();  
+        printStar4InLine();  
+        printStar5InLine(); 
+        printStar6InLine();
+        printStar7InLine();
+        printStar8InLine();
+        printStar9InLine();
+        printStar10InLine();
+    }
+
+    static void printStar1InLine() {
+        for (int i = 0; i < 1; i++) System.out.print('*');
+        System.out.println();
+    }
+
+    static void printStar2InLine() {
+        for (int i = 0; i < 2; i++) System.out.print('*');
+        System.out.println();
+    }
+
+    static void printStar3InLine() {
+        for (int i = 0; i < 3; i++) System.out.print('*');
+        System.out.println();
+    }
+
+    static void printStar4InLine() {
+        for (int i = 0; i < 4; i++) System.out.print('*');
+        System.out.println();
+    }
+
+    static void printStar5InLine() {
+        for (int i = 0; i < 5; i++) System.out.print('*');
+        System.out.println();
+    }
+
+    // -------- 실습과제 1) 추가 --------
+    static void printStar6InLine() {
+        for (int i = 0; i < 6; i++) System.out.print('*');
+        System.out.println();
+    }
+
+    static void printStar7InLine() {
+        for (int i = 0; i < 7; i++) System.out.print('*');
+        System.out.println();
+    }
+
+    static void printStar8InLine() {
+        for (int i = 0; i < 8; i++) System.out.print('*');
+        System.out.println();
+    }
+
+    static void printStar9InLine() {
+        for (int i = 0; i < 9; i++) System.out.print('*');
+        System.out.println();
+    }
+
+    static void printStar10InLine() {
+        for (int i = 0; i < 10; i++) System.out.print('*');
+        System.out.println();
+    }
+}
